bb01b941cd217c6cb2689fc8631917cd943b6174
mauridevops <contacto@mauriciooyola.com>
2022-01-09 20:24:47 -0300

    token

--- src/views/Stake/index.tsx
+++ src/views/Stake/index.tsx
@@ -103,7 +103,7 @@ function Stake() {
                     <Grid className="stake-card-grid" container direction="column" spacing={2}>
                         <Grid item>
                             <div className="stake-card-header">
-                                <p className="stake-card-header-title">Stake (🔱,🔱)</p>
+                                <p className="stake-card-header-title">Stake (☀️,☀️)</p>
                                 <RebaseTimer />
                             </div>
                         </Grid>
