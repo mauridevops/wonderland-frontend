19654120b9143b72e98294005bb75ea11380faac
mauridevops <contacto@mauriciooyola.com>
2022-01-12 05:50:26 -0300

    token price original

--- src/helpers/token-price.ts
+++ src/helpers/token-price.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 const cache: { [key: string]: number } = {};
 
 export const loadTokenPrices = async () => {
-    const url = "https://api.coingecko.com/api/v3/simple/price?ids=busd,tether&vs_currencies=usd";
+    const url = "https://api.coingecko.com/api/v3/simple/price?ids=terrausd,frax&vs_currencies=usd";
     const { data } = await axios.get(url);
 
-    cache["BUSD"] = data["terrausd"].usd;
-    cache["USDT"] = data["frax"].usd;
+    cache["UST"] = data["terrausd"].usd;
+    cache["FRAX"] = data["frax"].usd;
 };
 
 export const getTokenPrice = (symbol: string): number => {
